74230df5b426710420bf4408d3c10bf03a5e348b
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-03-31 19:27:17 +0200

    TP4 add 2

---
+++ TP4/TP4_CamReg/CamReg/process_image.h
@@ -0,0 +1,9 @@
+#ifndef PROCESS_IMAGE_H
+#define PROCESS_IMAGE_H
+
+float get_distance_cm(void);
+void process_image_start(void);
+uint16_t extract_line_width(uint8_t *buffer);
+uint16_t extract_line_width(uint8_t *buffer);
+
+#endif /* PROCESS_IMAGE_H */
