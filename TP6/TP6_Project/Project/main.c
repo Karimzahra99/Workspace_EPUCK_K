dda99c9598e3e30700f902417a68ffe656854c50
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-04-20 16:11:11 +0200

    TP6

---
+++ TP6/TP6_Project/Project/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <motors.h>
+#include <camera/po8030.h>
+#include <chprintf.h>
+#include <sensors/proximity.h>
+#include <pid_regulator.h>
+#include <process_image.h>
+
+//Functions for communication and visualization
+void SendUint8ToComputer(uint8_t* data, uint16_t size) 
+{
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+}
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    //Initialisation bus
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+    //starts the serial communication / can be removed if communication not needed
+    serial_start();
+    //start the USB communication / can be removed if communication not needed
+    usb_start();
+    //starts the camera
+    dcmi_start();
+	po8030_start();
+	//inits the motors
+	motors_init();
+
+	//stars the threads for the pi regulator and the processing of the image
+	//pi_regulator_start();
+	//process_image_start();
+
+	proximity_start();
+
+    /* Infinite loop. */
+    while (1) {
+    	//waits 1 second
+        chThdSleepMilliseconds(1000);
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
---
+++ TP6/TP6_Project/Project/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "camera/dcmi_camera.h"
+#include "msgbus/messagebus.h"
+#include "parameter/parameter.h"
+
+
+//constants for the differents parts of the project
+#define IMAGE_BUFFER_SIZE		640
+#define WIDTH_SLOPE				5
+#define MIN_LINE_WIDTH			40
+#define ROTATION_THRESHOLD		10
+#define ROTATION_COEFF			2
+#define PXTOCM					1570.0f //experimental value //line width at 10 cm
+#define GOAL_DISTANCE 			10.0f //REMOVE LATER
+#define MAX_DISTANCE 			25.0f //REMOVE LATER
+#define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
+#define KP						800.0f
+#define KI 						3.5f	//must not be zero
+#define KD						25f		//a tuner -> utiliser deuxieme methode de ZN avec Ku et Pu
+#define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
+
+
+/** Robot wide IPC bus. */
+extern messagebus_t bus;
+
+extern parameter_namespace_t parameter_root;
+
+void SendUint8ToComputer(uint8_t* data, uint16_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
