8c0185936032867b4caa54c16a5b735746808d7f
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-05-07 11:40:02 +0200

    .h fixes for new types

--- TP6/TP6_Project/Project/read_image.h
+++ TP6/TP6_Project/Project/read_image.h
@@ -9,7 +9,7 @@ void tune_image_start(tuning_config_t arg_tune_settings);
 
 uint8_t get_color(void);
 int16_t get_middle_diff(void);
-uint16_t get_middle_top(void);
-uint16_t get_middle_bot(void);
+int16_t get_middle_top(void);
+int16_t get_middle_bot(void);
 
 #endif /* READ_IMAGE_H_ */
