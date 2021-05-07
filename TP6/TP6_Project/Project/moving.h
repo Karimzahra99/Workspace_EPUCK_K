5c709ddcdfe15cf810e501a916ab77824067687a
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-05-07 13:18:02 +0200

    new names for pid_regulator.c

--- TP6/TP6_Project/Project/moving.h
+++ TP6/TP6_Project/Project/moving.h
@@ -1,10 +1,10 @@
-#ifndef PID_REGULATOR_H_
-#define PID_REGULATOR_H_
+#ifndef MOVING_H_
+#define MOVING_H_
 
-
+#include <stdint.h>
 //start the PID regulator thread
-void pid_regulator_start(void);
+void moving_start(void);
 
 void motor_set_position(float position_r, float position_l, int16_t speed_r, int16_t speed_l);
 
-#endif /* PID_REGULATOR_H_ */
+#endif /* MOVING_H_ */
