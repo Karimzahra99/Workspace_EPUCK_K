983306a2a729953fdc2d7cf43bbd14e8e4a64c14
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-05-10 13:14:24 +0200

    Update main.c

--- TP6/TP6_Project/Project/main.c
+++ TP6/TP6_Project/Project/main.c
@@ -67,12 +67,12 @@ int main(void)
 	 * send_params : NO_VISUALIZE_PARAMS, VISUALIZE_PARAMS
 	 */
 
-	rgb_gain_t rgb_gains = {94, 85, 90};
+	rgb_gain_t rgb_gains = {110, 85, 90};
 	uint8_t contrast = 64;
 	//tuning uses line_index_top for plot visualization
 	uint16_t line_index_top = 10;
 	detect_mode_t mode_detect = SUPER_RAINY_DAY;
-	visualize_mode_t send_params = NO_VISUALIZE_PARAMS;
+	visualize_mode_t send_params = YES_VISUALIZE_PARAMS;
 #ifdef TUNE
 	//chose which color intensity to plot with plot_image.py
 	color_index_t plot_pixels_color = RED_IDX;
