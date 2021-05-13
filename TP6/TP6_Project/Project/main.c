217765d1dbb2713a1371dea14f7248b4409892c3
Arthurepfl123 <arthur.lamour99@gmail.com>
2021-05-13 20:09:44 +0200

    Update main.c

--- TP6/TP6_Project/Project/main.c
+++ TP6/TP6_Project/Project/main.c
@@ -111,7 +111,7 @@ int main(void)
 
 	//playMelodyStart();
 
-	moving_start();
+	//moving_start();
 
 #endif
 
@@ -120,11 +120,11 @@ int main(void)
 	chThdSleepMilliseconds(1000);
 
 		//chprintf((BaseSequentialStream *)&SD3, "Mode =%-7d \r\n\n",get_rolling_mode());
-//		chprintf((BaseSequentialStream *)&SD3, "TOP =%-7d BOT =%-7d DIFF =%-7d COLOR =%-7d \r\n\n",
-//						get_middle_top(), get_middle_bot(), get_middle_diff(),get_color());
+		chprintf((BaseSequentialStream *)&SD3, "TOP =%-7d BOT =%-7d DIFF =%-7d COLOR =%-7d \r\n\n",
+						get_middle_top(), get_middle_bot(), get_middle_diff(),get_color());
 
-		chprintf((BaseSequentialStream *)&SD3, "DIFF =%-7d COLOR =%-7d Mode =%-7d \r\n\n"
-					, get_middle_diff(),get_color(),get_rolling_mode());
+//		chprintf((BaseSequentialStream *)&SD3, "DIFF =%-7d COLOR =%-7d Mode =%-7d \r\n\n"
+//					, get_middle_diff(),get_color(),get_rolling_mode());
 
 	}
 }
